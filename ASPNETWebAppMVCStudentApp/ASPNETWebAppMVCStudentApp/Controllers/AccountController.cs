37100a20940e801e03ed1ad77d599509d7e8404e
Phat <duyphathust@gmail.com>
2026-03-20 21:29:44 +0700

    Add new steps from exer2 and exer3

---
+++ ASPNETWebAppMVCStudentApp/ASPNETWebAppMVCStudentApp/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNETWebAppMVCStudentApp.Models;
+
+namespace ASPNETWebAppMVCStudentApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private SchoolDBLab06Entities db = new SchoolDBLab06Entities();
+
+        // Hiển thị form đăng nhập
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Username, string Password)
+        {
+            var user = db.tblUsers.FirstOrDefault(u => u.Username == Username && u.Password == Password);
+
+            if (user != null)
+            {
+                Session["LoggedUser"] = user.Username;
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Invalid Username or Password. Please try again.";
+                return View();
+            }
+        }
+    }
+}
---
+++ ASPNETWebAppMVCStudentApp/ASPNETWebAppMVCStudentApp/Models/PartialClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPNETWebAppMVCStudentApp.Models
+{
+    [MetadataType(typeof(PartialClasses))]
+    public partial class Student { }
+    public class PartialClasses
+    {
+        [Required(ErrorMessage = "First Name is required!")]
+        [StringLength(50, ErrorMessage = "First Name cannot exceed 50 characters.")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required!")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+    }
+}
